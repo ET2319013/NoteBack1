a234714fac8f81c6debe18176e009c1a3088341f
ET2319013 <2319013@vumk.eu>
2025-03-30 21:59:31 +0300

    +

--- DTOs/AuthDtos.cs
+++ DTOs/AuthDtos.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 
 namespace NotesManager.DTOs
 {
     public class RegisterDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
+    {    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
     }
 
     public class LoginDto
